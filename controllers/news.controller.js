c9e09deb9087e36f4220328b336afd8696a25062
nczkevin <z1376871863@163.com>
2016-07-25 01:01:52 +0800

    news

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,5 +1,6 @@
-var request = require('superagent');
 var cheerio = require('cheerio');
+var charset = require('superagent-charset');
+var request = charset(require('superagent'));
 
 exports.txskNews = function(req,res){
 // 团学速递：http://news.youth.ncu.edu.cn/news/?list_11.html
@@ -8,22 +9,69 @@ exports.txskNews = function(req,res){
   var txsd = [],
       yxdt = [],
       tzgg = [];
+  var baseUrl = "http://news.youth.ncu.edu.cn"
   request
     .get('http://news.youth.ncu.edu.cn/news/?list_11.html')
+    .charset('gbk')
     .end(function(err,sres){
       if (err) {
         res.json(err);
       }
       var $ = cheerio.load(sres.text);
-      $('.listArrow1 > ul > ul > li').each(function(i,elem){
-        console.log(elem);
+      $('.listArrow1  li > a').each(function(i,elem){
         var $elem = $(elem);
-        // console.log($elem);
-        txsd.push({
-          title: $elem.attr('title'),
-          href: $elem.attr('href')
-        });
+        if (i <= 5) {
+          txsd.push({
+            title: $elem.attr('title'),
+            href: baseUrl + $elem.attr('href').substr(2)
+          });
+        }
       });
-      res.json(txsd);
+      request
+        .get('http://news.youth.ncu.edu.cn/news/?list_12.html')
+        .charset('gbk')
+        .end(function(err,sres){
+          if (err) {
+            res.json(err);
+          }
+          var $ = cheerio.load(sres.text);
+          $('.listArrow1  li > a').each(function(i,elem){
+            var $elem = $(elem);
+            if (i <= 5) {
+              yxdt.push({
+                title: $elem.attr('title'),
+                href: $elem.attr('href')
+              });
+            }
+          });
+          request
+            .get('http://news.youth.ncu.edu.cn/news/?list_13.html')
+            .charset('gbk')
+            .end(function(err,sres){
+              if (err) {
+                res.json(err);
+              }
+              var $ = cheerio.load(sres.text);
+              $('.listArrow1  li > a').each(function(i,elem){
+                var $elem = $(elem);
+                if (i <= 5) {
+                  tzgg.push({
+                    title: $elem.attr('title'),
+                    href: $elem.attr('href')
+                  });
+                }
+              });
+              var txsknews = {
+                txsd: txsd,
+                yxdt: yxdt,
+                tzgg: tzgg
+              };
+
+              res.json(txsknews);
+            });
+        });
     });
+
+
+
 };
